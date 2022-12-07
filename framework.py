63c1c40094c9d912db38acb04408bedd857983c6
adventcoder <59347361+adventcoder@users.noreply.github.com>
2022-12-07 22:07:45 +0000

    Add support for custom inputs

--- framework.py
+++ framework.py
@@ -1,17 +1,32 @@
 
-import __main__, re, os, time
+import __main__, argparse, os, sys, time, math
 
 def main(input = None):
-  if input is None:
-      input = get_input(__main__)
-  print_answers(__main__.solve, input)
+    args = parse_args()
+    if input is None:
+        input = get_input(args)
+    print_answers(__main__.solve, input)
 
-def get_input(mod):
-    return read_input(get_day_number(mod))
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input', help = 'path to custom input file (use "-" for stdin)')
+    return parser.parse_args()
 
-def get_day_number(mod):
-    name = os.path.basename(mod.__file__)
-    return int(re.search(r'\d+', name).group())
+def get_input(args, mod = __main__):
+    if args.input is not None:
+        if args.input == '-':
+            return sys.stdin.read()
+        else:
+            return read_input(args.input)
+    return read_input(get_input_path(mod))
+
+def get_input_path(mod):
+    name = os.path.basename(mod.__file__).replace('.py', '.txt')
+    return os.path.join(os.path.dirname(mod.__file__), 'inputs', name)
+
+def read_input(path):
+    with open(path, 'r', encoding = 'utf8') as file:
+        return file.read()
 
 def print_answers(solver, input):
     start_time = time.perf_counter()
@@ -24,12 +39,10 @@ def print_answer(n, answer, answer_time):
     print('Part {}: {} [{}]'.format(n, answer, format_time(answer_time)))
 
 def format_time(time):
+    seconds = math.floor(time)
+    milliseconds = (time - seconds) * 1000
     parts = []
-    if time >= 1:
-        parts.append('%d s' % time)
-    parts.append('%.3f ms' % (time * 1000))
+    if seconds:
+        parts.append('%d s' % seconds)
+    parts.append('%.3f ms' % milliseconds)
     return ' '.join(parts)
-
-def read_input(day):
-    with open('inputs/day{}.txt'.format(day), 'r', encoding = 'utf8') as file:
-        return file.read()
