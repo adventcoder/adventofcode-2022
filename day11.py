491795402e80f311ba31dc1c3afd8ef5a1046f34
adventcoder <59347361+adventcoder@users.noreply.github.com>
2022-12-11 18:01:43 +0000

    Optimised

--- day11.py
+++ day11.py
@@ -1,57 +1,54 @@
 
 import framework
-from utils import product
+from collections import deque
+from functools import reduce
+import math
 
 def solve(input):
-    yield part1(input)
-    yield part2(input)
+    monkeys = tuple(map(Monkey, input.split('\n\n')))
+    yield monkey_business(monkeys, 20, lambda worry: worry // 3)
+    modulus = reduce(math.lcm, [monkey.test for monkey in monkeys], 1)
+    yield monkey_business(monkeys, 10000, lambda worry: worry % modulus)
 
-def part1(input):
-    troop = list(map(Monkey, input.split('\n\n')))
-    for _ in range(20):
-        for monkey in troop:
-           monkey.turn(troop, lambda worry: worry // 3)
-    troop.sort(key = lambda m: m.inspected)
-    return troop[-1].inspected * troop[-2].inspected
-
-def part2(input):
-    troop = list(map(Monkey, input.split('\n\n')))
-    modulus = product(monkey.test for monkey in troop)
-    for _ in range(10000):
-        for monkey in troop:
-           monkey.turn(troop, lambda worry: worry % modulus)
-    troop.sort(key = lambda m: m.inspected)
-    return troop[-1].inspected * troop[-2].inspected
+def parse_op(expr):
+    tokens = expr.split()
+    assert tokens[0] == 'old'
+    if tokens[2] == tokens[0]:
+        return { '+': lambda old: old + old, '*': lambda old: old * old }[tokens[1]]
+    else:
+        x = int(tokens[2])
+        return { '+': lambda old: old + x, '*': lambda old: old * x }[tokens[1]]
 
 class Monkey:
     def __init__(self, chunk):
         for line in chunk.splitlines():
             label, value = line.lstrip().split(':', 2)
             if label == 'Starting items':
-                self.items = list(map(int, value.split(',')))
+                self.starting_items = list(map(int, value.split(',')))
             elif label == 'Operation':
-                self.op = value.strip()
+                self.inspect = parse_op(value.split('=')[1])
             elif label == 'Test':
                 self.test = int(value.split()[-1])
             elif label == 'If true':
                 self.index_if_true = int(value.split()[-1])
             elif label == 'If false':
                 self.index_if_false = int(value.split()[-1])
-        self.inspected = 0
 
-    def operate(self, item):
-        vars = { 'old': item }
-        exec(self.op, None, vars)
-        return vars['new']
+    def throw_index(self, item):
+        return self.index_if_true if item % self.test == 0 else self.index_if_false
 
-    def turn(self, troop, relief_management):
-        while self.items:
-            item = relief_management(self.operate(self.items.pop(0)))
-            self.inspected += 1
-            if item % self.test == 0:
-                troop[self.index_if_true].items.append(item)
-            else:
-                troop[self.index_if_false].items.append(item)
+def monkey_business(monkeys, rounds, relief):
+    queues = [deque(monkey.starting_items) for monkey in monkeys]
+    inspections = [0] * len(monkeys)
+    for _ in range(rounds):
+        for i, monkey in enumerate(monkeys):
+            while queues[i]:
+                item = queues[i].popleft()
+                new_item = relief(monkey.inspect(item))
+                inspections[i] += 1
+                queues[monkey.throw_index(new_item)].append(new_item)
+    inspections.sort(reverse = True)
+    return inspections[0] * inspections[1]
 
 if __name__ == '__main__':
     framework.main()
