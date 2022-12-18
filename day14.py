bb7a281b305ade69be614df75b2d24ff84f5a098
adventcoder <59347361+adventcoder@users.noreply.github.com>
2022-12-18 16:19:27 +0000

    Cleanup

--- day14.py
+++ day14.py
@@ -1,68 +1,38 @@
 import framework
 from itertools import pairwise
-from contextlib import contextmanager
-import gif
 
-def solve(input, gif_path = None):
-    grid = parse_grid(input)
-    max_y = max(y for _, y in grid.keys())
+def solve(input):
+    walls = parse_walls(input)
+    max_y = max(y for _, y in walls)
     floor_y = max_y + 2
-    start = (500, 0)
-    with open_graphics(gif_path, grid, start, floor_y) as graphics:
-        fill(grid, start, floor_y, max_y, graphics)
-        yield sum(c == 'o' for c in grid.values())
-        fill(grid, start, floor_y, floor_y, graphics)
-        yield sum(c == 'o' for c in grid.values())
+    yield len(fill(walls, 500, 0, floor_y, max_y))
+    yield len(fill(walls, 500, 0, floor_y, floor_y))
 
-def parse_grid(input):
-    grid = {}
+def parse_walls(input):
+    walls = set()
     for line in input.splitlines():
         coords = [tuple(int(s) for s in coords.split(',')) for coords in line.split('->')]
         for (x0, y0), (x1, y1) in pairwise(coords):
             for y in range(min(y0, y1), max(y0, y1) + 1):
                 for x in range(min(x0, x1), max(x0, x1) + 1):
-                    grid[(x, y)] = '#'
-    return grid
+                    walls.add((x, y))
+    return walls
 
-def fill(grid, start, floor_y, max_y, graphics = None):
+def fill(walls, start_x, start_y, floor_y, max_y):
+    sand = set()
     path = []
-    pos = start
+    pos = (start_x, start_y)
     while pos and pos[1] < max_y:
         for dx in (0, -1, 1):
             new_pos = (pos[0] + dx, pos[1] + 1)
-            if new_pos not in grid and new_pos[1] < floor_y:
+            if new_pos not in walls and new_pos not in sand and new_pos[1] < floor_y:
                 path.append(pos)
                 pos = new_pos
                 break
         else:
-            grid[pos] = 'o'
-            if graphics is not None:
-                render_grid(graphics, grid, start, floor_y, pos[0], pos[1], pos[0] + 1, pos[1] + 1)
+            sand.add(pos)
             pos = path.pop() if path else None
-
-@contextmanager
-def open_graphics(path, grid, start, floor_y):
-    if path is not None:
-        x0, y0 = start[0] - floor_y, start[1]
-        x1, y1 = start[0] + floor_y + 1, start[1] + floor_y
-        colors = [0x000000, 0xD77A69, 0xCDAA6D]
-        with gif.open_graphics(path, gif.ScreenDescriptor(x1 - x0, y1 - y0, colors)) as graphics:
-            render_grid(graphics, grid, start, floor_y, x0, y0, x1, y1)
-            yield graphics
-    else:
-        yield None
-
-def render_grid(graphics, grid, start, floor_y, x0, y0, x1, y1):
-    screen_x0, screen_y0 = start[0] - floor_y, start[1]
-    image = gif.Image(x0 - screen_x0, y0 - screen_y0, x1 - x0, y1 - y0)
-    for y in range(y0, y1):
-        for x in range(x0, x1):
-            c = grid.get((x, y), '.')
-            if c == 'o':
-                image.set_pixel(x - x0, y - y0, 2)
-            elif c == '#':
-                image.set_pixel(x - x0, y - y0, 1)
-    graphics.render(image, delay = 0.01)
+    return sand
 
 if __name__ == '__main__':
     framework.main()
