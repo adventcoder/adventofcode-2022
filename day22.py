d1b2833eeeff5002a3fa4a2f4a851cc9b6a57cee
adventcoder <59347361+adventcoder@users.noreply.github.com>
2022-12-22 19:56:44 +0000

    cleaned up but wrong

--- day22.py
+++ day22.py
@@ -1,108 +1,100 @@
 import framework
-           # right, down, left, up
+import re
+
 facings = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
-# TODO HARDCODED PART 2 AND DELETED PART 1
+tile_width = 50
+tile_height = 50
 
 def solve(input):
     chunks = input.split('\n\n')
     grid = [list(line) for line in chunks[0].splitlines()]
     path = parse_path(chunks[1])
-
-    transfer = {}
-    for i in range(50):
-        transfer[(50 + i, 0, 3)] = (0, 150 + i, 0)
-        transfer[(0, 150 + i, 2)] = (50 + i, 0, 1)
-    for i in range(50):
-        transfer[(50, 50 - 1 - i, 2)] = (0, 100 + i, 0)
-        transfer[(0, 100 + i, 2)] = (50, 50 - 1 - i, 0)
-    for i in range(50):
-        transfer[(50 - 1 - i, 200 - 1, 1)] = (150 - 1 - i, 0, 1)
-        transfer[(150 - 1 - i, 0, 3)] = (50 - 1 - i, 200 - 1, 3)
-    for i in range(50):
-        transfer[(150 - 1, 50 - 1 - i, 0)] = (100 - 1, 100 + i, 2)
-        transfer[(100 - 1, 100 + i, 0)] = (150 - 1, 50 - 1 - i, 2)
-    for i in range(50):
-        transfer[(50 - 1, 200 - 1 - i, 0)] = (100 - 1 - i, 150 - 1, 3)
-        transfer[(100 - 1 - i, 150 - 1, 1)] = (50 - 1, 200 - 1 - i, 2)
-    for i in range(50):
-        transfer[(50, 50 + i, 2)] = (i, 100, 1)
-        transfer[(i, 100, 3)] = (50, 50 + i, 0)
-    for i in range(50):
-        transfer[(100 - 1, 100 - 1 - i, 0)] = (150 - 1 - i, 50 - 1, 3)
-        transfer[(150 - 1 - i, 50 - 1, 1)] = (100 - 1, 100 - 1 - i, 2)
-
-    yield follow(grid, path, transfer)
-
-def putsym(h, k, v):
-    h[k] = v
-    h[v] = k
-
-def follow(grid, path, transfer):
-    x = incx(grid, -1, 0, 1)
-    y = 0
-    dx = 1
-    dy = 0
-    for arg in path:
-        if arg == 'L':
-            dx, dy, = dy, -dx
-            grid[y][x] = '>v<^'[facings.index((dx, dy))]
-        elif arg == 'R':
-            dx, dy = -dy, dx
-            grid[y][x] = '>v<^'[facings.index((dx, dy))]
-        else:
-            for _ in range(arg):
-                d = facings.index((dx, dy))
-                if (x, y, d) in transfer:
-                    next_x, next_y, next_d = transfer[(x, y, d)]
-                else:
-                    next_x = x + dx
-                    next_y = y + dy
-                    next_d = d
-                print(x, y, d, next_x, next_y)
-                if grid[next_y][next_x] == '#':
-                    break
-                x = next_x
-                y = next_y
-                dx, dy = facings[next_d]
-                grid[y][x] = '>v<^'[facings.index((dx, dy))]
-    return password(x, y, dx, dy)
+    #yield password(grid, path, wrap1)
+    yield password(grid, path, wrap2)
 
 def parse_path(chunk):
-    tokens = chunk.replace('L', ' L ').replace('R', ' R ').split()
-    return (int(token) if token.isdigit() else token for token in tokens)
+    return (int(token) if token.isdigit() else token for token in re.findall(r'\d+|L|R', chunk.strip()))
 
-def password(x, y, dx, dy):
-    return 1000 * (y + 1) + 4 * (x + 1) + facings.index((dx, dy))
+def password(grid, path, wrap):
+    x, y, d = wrap1(grid, len(grid[0]) - 1, 0, 0)
+    for arg in path:
+        if arg == 'L':
+            d = (d - 1) % len(facings)
+        elif arg == 'R':
+            d = (d + 1) % len(facings)
+        else:
+            for _ in range(arg):
+                dx, dy = facings[d]
+                next_x = x + dx
+                next_y = y + dy
+                next_d = d
+                if not in_bounds(grid, next_x, next_y):
+                    print(f'pre wrap: ({x}, {y}, {d})')
+                    next_x, next_y, next_d = wrap(grid, x, y, d)
+                    print(f'post wrap: ({next_x}, {next_y}, {next_d})')
+                if grid[next_y][next_x] == '#':
+                    break
+                x, y, d = next_x, next_y, next_d
+    return 1000 * (y + 1) + 4 * (x + 1) + d
 
-def incx(grid, x, y, dx):
+def in_bounds(grid, x, y):
+    return 0 <= y < len(grid) and 0 <= x < len(grid[y]) and grid[y][x] != ' '
+
+def wrap1(grid, x, y, d):
+    dx, dy = facings[d]
+    # Loop over tiles instead of individual pixels
+    tile_xcount = len(grid[y]) // tile_width
+    tile_ycount = len(grid) // tile_height
     x = (x + dx) % len(grid[y])
-    while grid[y][x] == ' ':
-        x = (x + dx) % len(grid[y])
-    return x
-
-def incy(grid, x, y, dy):
     y = (y + dy) % len(grid)
-    while x >= len(grid[y]) or grid[y][x] == ' ':
-        y = (y + dy) % len(grid)
-    return y
+    tile_x, ox = divmod(x, tile_width)
+    tile_y, oy = divmod(y, tile_height)
+    while not in_bounds(grid, tile_x * tile_width, tile_y * tile_height):
+        tile_x = (tile_x + dx) % tile_xcount
+        tile_y = (tile_y + dy) % tile_ycount
+    return tile_x * tile_width + ox, tile_y * tile_height + oy, d
 
+# right, down, left, up
+def wrap2(grid, x, y, d):
+    # TODO: not hardcode this (never going to happen)
+    tile_x, ox = divmod(x, tile_width)
+    tile_y, oy = divmod(y, tile_height)
+    if tile_x == 1 and tile_y == 0:
+        if d == 2:
+            return (0, 3 * tile_height - 1 - oy, 0)
+        elif d == 3:
+            return (0, 3 * tile_height + ox, 0)
+    elif tile_x == 2 and tile_y == 0:
+        if d == 0:
+            return (2 * tile_width - 1, 3 * tile_height - 1 - oy, 2)
+        elif d == 1:
+            return (2 * tile_width - 1, tile_height + oy, 2)
+        elif d == 3:
+            return (ox, 4 * tile_height - 1, 3)
+    elif tile_x == 1 and tile_y == 1:
+        if d == 0:
+            return (2 * tile_width + oy, tile_height - 1, 3)
+        elif d == 2:
+            return (oy, 2 * tile_height, 1)
+    elif tile_x == 0 and tile_y == 2:
+        if d == 2:
+            return (tile_width, tile_height - 1 - oy, 0)
+        elif d == 3:
+            return (tile_width, tile_height + ox, 0)
+    elif tile_x == 1 and tile_y == 2:
+        if d == 0:
+            return (3 * tile_width - 1, tile_height - 1 - oy, 2)
+        elif d == 1:
+            return (tile_width - 1, 3 * tile_height + ox, 2)
+    elif tile_x == 0 and tile_y == 3:
+        if d == 0:
+            return (tile_width + oy, 3 * tile_height - 1, 3)
+        elif d == 1:
+            return (2 * tile_width + ox, 0, 1)
+        elif d == 2:
+            return (tile_width + oy, 0, 1)
+    assert False
 
 if __name__ == '__main__':
-    example = '''\
-        ...#
-        .#..
-        #...
-        ....
-...#.......#
-........#...
-..#....#....
-..........#.
-        ...#....
-        .....#..
-        .#......
-        ......#.
-
-10R5L5R10L4R5L5
-'''
     framework.main()
