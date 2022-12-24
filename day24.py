1d03ee272162077b0c8c3f7203aa15d1b66acba1
adventcoder <59347361+adventcoder@users.noreply.github.com>
2022-12-24 08:18:43 +0000

    Cleanup

--- day24.py
+++ day24.py
@@ -1,63 +1,78 @@
 import framework
+from math import *
 from collections import deque
-from math import lcm
 
 def solve(input):
-    grid = [list(line) for line in input.splitlines()]
-
-    blizzards = parse_blizzards(grid)
-    start_y = 0
-    start_x = grid[start_y].index('.')
-    end_y = len(grid) - 1
-    end_x = grid[end_y].index('.')
-
-    time = find_path(grid, blizzards, start_x, start_y, end_x, end_y, 0)
+    width, height, blizzards = parse_grid(input)
+    start = (0, 0)
+    end = (width - 1, height - 1)
+    time = find_path(width, height, blizzards, start, end)
     yield time
-    time = find_path(grid, blizzards, end_x, end_y, start_x, start_y, time)
-    time = find_path(grid, blizzards, start_x, start_y, end_x, end_y, time)
+    time = find_path(width, height, blizzards, end, start, time)
+    time = find_path(width, height, blizzards, start, end, time)
     yield time
 
-def parse_blizzards(grid):
-    cycle_time = lcm(len(grid) - 2, len(grid[0]) - 2)
+def parse_grid(input):
+    lines = input.splitlines()
+    width = len(lines[0]) - 2
+    height = len(lines) - 2
+    cycle_time = lcm(width, height)
     blizzards = [set() for _ in range(cycle_time)]
-    for y in range(1, len(grid) - 1):
-        for x in range(1, len(grid[y]) - 1):
-            for z in range(cycle_time):
-                if grid[y][x] == '^':
-                    blizzards[z].add((x, (y - 1 - z) % (len(grid) - 2) + 1))
-                elif grid[y][x] == 'v':
-                    blizzards[z].add((x, (y - 1 + z) % (len(grid) - 2) + 1))
-                elif grid[y][x] == '>':
-                    blizzards[z].add(((x - 1 + z) % (len(grid[y]) - 2) + 1, y))
-                elif grid[y][x] == '<':
-                    blizzards[z].add(((x - 1 - z) % (len(grid[y]) - 2) + 1, y))
-    return blizzards
+    for y, line in enumerate(lines[1:-1]):
+        for x, c in enumerate(line[1:-1]):
+            if c != '.':
+                for i in range(cycle_time):
+                    blizzards[i].add((x, y))
+                    if c == '^':
+                        y = (y - 1) % height
+                    elif c == 'v':
+                        y = (y + 1) % height
+                    elif c == '>':
+                        x = (x + 1) % width
+                    elif c == '<':
+                        x = (x - 1) % width
+    return width, height, blizzards
 
-def find_path(grid, blizzards, start_x, start_y, end_x, end_y, start_time):
-    start = (start_x, start_y, start_time % len(blizzards))
-    queue = deque([start])
-    time = { start: start_time }
+def find_path(width, height, blizzards, start, end, time = 0):
+    # wait for start to be clear then enter the maze
+    time += 1
+    while start in blizzards[time % len(blizzards)]:
+        time += 1
+    queue = deque()
+    seen = set()
+    queue.append((start, time))
+    seen.add((start, time % len(blizzards)))
     while queue:
-        x, y, z = queue.popleft()
-        if x == end_x and y == end_y:
-            return time[(x, y, z)]
+        pos, time = queue.popleft()
+        if pos == end:
+            # add extra minute to exit the maze
+            return time + 1
         else:
-            next_time = time[(x, y, z)] + 1
-            next_z = next_time % len(blizzards)
-            for next_x, next_y in neighbours(grid, x, y):
-                next = (next_x, next_y, next_z)
-                if next not in time and grid[next_y][next_x] != '#' and not (next_x, next_y) in blizzards[next_z]:
-                    time[next] = next_time
-                    queue.append(next)
+            i = (time + 1) % len(blizzards)
+            for new_pos in neighbours(pos, width, height):
+                if new_pos not in blizzards[i] and (new_pos, i) not in seen:
+                    seen.add((new_pos, i))
+                    queue.append((new_pos, time + 1))
 
-def neighbours(grid, x, y):
+def neighbours(pos, width, height):
+    x, y = pos
     yield x, y # wait
     if y > 0:
         yield x, y - 1
-    yield x - 1, y
-    yield x + 1, y
-    if y < len(grid) - 1:
+    if x > 0:
+        yield x - 1, y
+    if x < width - 1:
+        yield x + 1, y
+    if y < height - 1:
         yield x, y + 1
 
 if __name__ == '__main__':
+    example = '''\
+#.######
+#>>.<^<#
+#.<..<<#
+#>v.><>#
+#<^v^^>#
+######.#
+'''
     framework.main()
