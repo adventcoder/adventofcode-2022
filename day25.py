1161aa448b68c8eb87a54639c45915101682c11c
adventcoder <59347361+adventcoder@users.noreply.github.com>
2022-12-25 19:42:31 +0000

    Shorter code

--- day25.py
+++ day25.py
@@ -6,25 +6,14 @@ def solve(input):
 def parse_snafu(s):
     n = 0
     for c in s.strip():
-        if c == '=':
-            d = -2
-        elif c == '-':
-            d = -1
-        else:
-            d = int(c)
-        n = 5 * n + d
+        n = n * 5 + ('=-012'.index(c) - 2)
     return n
 
 def format_snafu(n):
     cs = []
-    while n > 0:
+    while n != 0:
         d = (n + 2) % 5 - 2
-        if d == -2:
-            cs.append('=')
-        elif d == -1:
-            cs.append('-')
-        else:
-            cs.append(str(d))
+        cs.append('=-012'[d + 2])
         n = (n - d) // 5
     return ''.join(reversed(cs))
 
